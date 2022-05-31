e5c6e959eb774d27fa9a31dbf26fc82f6d9074d7
Dany Dodson GIT_AUTHOR_EMAIL=danydodson@gmail.com GITHUB_TOKEN=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 HOMEBREW_TOKEN=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 <danydodson@gmail.com>
2022-05-31 13:41:43 -0500

    ✨ added variables

--- scripts/daily/clean.sh
+++ scripts/daily/clean.sh
@@ -8,13 +8,13 @@ __info 'Removes DS_Stores'
 find . -name '*.DS_Store' -type d -prune -exec echo '{}' \; -exec rm -rf {} \;
 
 __info 'Removes node_modules'
-find ~/Documents/Developer -name 'node_modules' -type d -prune -exec echo '{}' \; -exec rm -rf {} \;
+find ~/Developer -name 'node_modules' -type d -prune -exec echo '{}' \; -exec rm -rf {} \;
 
 # __info 'Removes logs'
 # sudo rm -rfv /private/var/log/asl/*.asl
 
 # __info 'Removes compdump'
-rm -rfv ~/.config/zsh/.zcompdump*
+# rm -rfv ~/.config/zsh/.zcompdump*
 
 __info 'Removes Trash'
 sudo rm -rfv /Volumes/*/.Trashes
