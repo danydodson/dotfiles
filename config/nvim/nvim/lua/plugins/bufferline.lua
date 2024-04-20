d5491d41db91cced88f288f1901bf6c04540b4a5
danydodson <danydodson@gmail.com>
2024-04-19 20:12:04 -0500

    Updates

--- config/nvim/nvim/lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "akinsho/bufferline.nvim",
-    keys = {
-      { "[b", "<cmd>BufferLineMovePrev<cr>", desc = "Buffer move prev" },
-      { "]b", "<cmd>BufferLineMoveNext<cr>", desc = "Buffer move next" },
-    },
-  },
-}
