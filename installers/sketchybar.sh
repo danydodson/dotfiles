ef524ec78edee4b38a9734937e42fe538958afee
danydodson <danydodson@gmail.com>
2025-05-29 03:29:20 -0500

    updates

--- installers/sketchybar.sh
+++ installers/sketchybar.sh
@@ -34,13 +34,7 @@ install_sketchybar() {
     # sbarlua
     (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
-    info "cloning config"
-    git clone https://github.com/FelixKratz/dotfiles.git /tmp/dotfiles
-    mv "$HOME"/.config/sketchybar "$HOME"/.config/sketchybar_backup
-    mv /tmp/dotfiles/.config/sketchybar "$HOME"/.config/sketchybar
-    rm -rf /tmp/dotfiles
     brew services restart sketchybar
-
 }
 
 main() {
