b167a064f4bfb8a278077269015db2936a0a86cf
danydodson <danydodson@gmail.com>
2025-06-03 18:32:03 -0500

    Updates

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -95,3 +95,22 @@ Tab.build = function(self, ...)
 	old_build(self, ...)
 end
 
+-- mime ext -----------------------------------------------------------------------------------------------------------
+
+require("mime-ext"):setup({
+	-- Expand the existing filename database (lowercase), for example:
+	with_files = {
+		makefile = "text/makefile",
+		-- ...
+	},
+
+	-- Expand the existing extension database (lowercase), for example:
+	with_exts = {
+		mk = "text/makefile",
+		-- ...
+	},
+
+	-- If the mime-type is not in both filename and extension databases,
+	-- then fallback to Yazi's preset `mime` plugin, which uses `file(1)`
+	fallback_file1 = false,
+})
