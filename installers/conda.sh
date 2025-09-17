32e24ab97c5961169b322ad32511d83ba49f78af
danydodson <danydodson@gmail.com>
2025-09-16 22:31:38 -0500

    install miniconda3

--- installers/conda.sh
+++ installers/conda.sh
@@ -18,25 +18,40 @@ install_miniconda() {
 
     info "running miniconda.sh..."
     bash ~/.config/miniconda3/miniconda.sh -b -u -p ~/.config/miniconda3
+  fi
 
-    source $HOME/.zshrc
+  if [ -d "$HOME/.config/miniconda3" ]; then
+    source "$HOME/.config/miniconda3/etc/profile.d/conda.sh"
 
-    # python packages (mainly for data science)
-    conda install -c apple tensorflow-deps
-    conda install -c conda-forge pybind11
-    conda install matplotlib
-    conda install jupyterlab
-    conda install seaborn
-    conda install opencv
-    conda install joblib
-    conda install pytables
+    if ! conda env list | grep -q "^myenv "; then
+      info "conda create myenv env 3.13.5 in miniconda..."
+      conda create -y -n myenv python=3.13.5
+    else
+      info "conda myenv environment already exists, skipping creation..."
 
-    pip install tensorflow-macos
-    pip install tensorflow-metal
-    pip install debugpy
-    pip install sklearn
+      info "conda activate myenv environment..."
+      conda activate myenv
 
-    echo
+      info "conda install packages in myenv environment..."
+      # conda install -c apple tensorflow-deps
+      # conda install -c conda-forge pybind11
+      for pkg in matplotlib jupyterlab seaborn opencv joblib pytables; do
+        if conda list "$pkg" | grep -q "^$pkg"; then
+          info "conda package '$pkg' already installed, skipping..."
+        else
+          conda install -y "$pkg"
+        fi
+      done
+
+      if pip show "debugpy" >/dev/null 2>&1; then
+        info "pip package 'debugpy' already installed, skipping..."
+      else
+        pip install "debugpy"
+      fi
+
+    fi
+  else
+    info "miniconda3 not found in ~/.config/miniconda3. Skipping conda environment setup..."
   fi
 }
 
