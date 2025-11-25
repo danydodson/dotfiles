08387b0129e399fef16c53aaefa0d3225c33c780
danydodson <danydodson@gmail.com>
2025-11-25 00:32:46 -0600

    added extra mpv configs

---
+++ config/mpv/scripts/filter_audio_scaletempo.lua
@@ -0,0 +1,38 @@
+-- deus0ww - 2022-08-18
+
+local mp = require 'mp'
+local msg = require 'mp.msg'
+local utils = require 'mp.utils'
+
+local filter_name = 'ScaleTempo'
+
+local function show_status(filter_enabled)
+  local filter_params = 'none'
+  for _, f in ipairs(mp.get_property_native 'af') do
+    if f.label == filter_name then
+      filter_params = f.name .. ' ' .. utils.to_string(f.params):gsub('{', '['):gsub('}', ']'):gsub(' = ', '='):gsub('"', ''):gsub(',', '')
+      break
+    end
+  end
+  mp.osd_message(string.format('Speed: %.2f\n%s %s:  %s', mp.get_property 'speed', (filter_enabled and '■' or '□'), filter_name, filter_params))
+end
+
+mp.register_script_message(filter_name .. '-enabled', function()
+  mp.commandv('set', 'audio-pitch-correction', 'yes')
+  show_status(true)
+end)
+mp.register_script_message(filter_name .. '-disabled', function()
+  mp.commandv('set', 'audio-pitch-correction', 'no')
+  show_status(false)
+end)
+
+local previous_speed = mp.get_property_native 'speed'
+
+mp.observe_property('speed', 'number', function(_, speed)
+  if not speed or speed == previous_speed then
+    return
+  end
+  msg.debug('AutoScaleTempo - Speed Changed - ', speed)
+  previous_speed = speed
+  mp.command_native { 'script-message', filter_name .. ((speed == 1) and '-disable' or '-enable'), 'yes' }
+end)
---
+++ config/mpv/scripts/test.lua
@@ -0,0 +1,47 @@
+local mp = require 'mp'
+local msg = require 'mp.msg'
+local opt = require 'mp.options'
+local utils = require 'mp.utils'
+
+local function show_value(label, value)
+  msg.debug(label .. ': ', value)
+  mp.osd_message(label .. ': ' .. utils.to_string(value))
+end
+
+local get = mp.get_property_native
+
+mp.register_script_message('get-audio-device-list', function()
+  show_value('audio-device-list', utils.to_string(get('audio-device-list', {})))
+end)
+
+mp.register_script_message('get-hidpi-scale', function()
+  show_value('display-hidpi-scale', get('display-hidpi-scale', -1.0))
+end)
+
+mp.register_script_message('get-path', function()
+  show_value('get-path: ', get('path', 'x'))
+end)
+
+mp.register_script_message('get-extension', function()
+  show_value('get-extension: ', get('filename', 'x'):match '^.+%.(.+)$')
+end)
+
+mp.register_script_message('get-protocol', function()
+  show_value('get-protocol: ', (get('path', 'x'):match '^(.+)://.+' or ''))
+end)
+
+mp.register_script_message('get-demuxer-via-network', function()
+  show_value('get-demuxer-via-network: ', (get 'demuxer-via-network' and 'yes' or 'no'))
+end)
+
+mp.register_script_message('get-sub-lang', function()
+  show_value('get-sub-lang', get('current-tracks/sub/lang', ''):lower())
+end)
+
+mp.observe_property('current-tracks/sub/lang', 'native', function(_, lang)
+  msg.debug(lang)
+end)
+
+mp.register_script_message('get-sub-type', function()
+  show_value('get-sub-lang', get('current-tracks/sub/codec', ''):lower())
+end)
