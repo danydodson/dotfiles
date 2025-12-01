5a4bd4de999ff822b0b925df2f87623b099bca44
danydodson <danydodson@gmail.com>
2025-12-01 14:12:53 -0600

    clean

--- config/codium/apc/main.js
+++
@@ -1,7 +0,0 @@
-//  create a frameless window
-// const { BrowserWindow } = require('electron')
-
-//  hides the traffic lights
-// const win2 = new BrowserWindow({ titleBarStyle: 'hidden' })
-// win2.setWindowButtonVisibility(false)
-
--- config/codium/custom/main.js
+++ config/codium/custom/main.js
@@ -1,9 +1,9 @@
-//  create a frameless window
-const { BrowserWindow } = require('electron')
+// //  create a frameless window
+// const { BrowserWindow } = require('electron')
 
-//  hides the traffic lights
-const win2 = new BrowserWindow({ titleBarStyle: 'hidden' })
-win2.setWindowButtonVisibility(false)
+// //  hides the traffic lights
+// const win2 = new BrowserWindow({ titleBarStyle: 'hidden' })
+// win2.setWindowButtonVisibility(false)
 
 // document.addEventListener('DOMContentLoaded', function () {
 //     const checkElement = setInterval(() => {
